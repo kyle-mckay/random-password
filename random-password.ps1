eb5b095e0175292b8dff694a20511adb60e2d534
Kyle <83886427+kyle-mckay@users.noreply.github.com>
2024-01-30 08:57:13 -0700

    Add additional generation method
    Allows the script to be set to allow generation until prompted to stop OR to generate up to a limit.

--- random-password.ps1
+++ random-password.ps1
@@ -1,4 +1,4 @@
-# section
+#region function
 function GetRandomPassword () {
     <# 
         Programmed by Christian Haensel
@@ -10,18 +10,18 @@ function GetRandomPassword () {
         long as this information remains intact.
         2010-12-30 Modified by Josh Hartman
         2019-07-30 Modified by Joseph Kreifels II  - Now with compound letters, special characters, capital letters. Passwords can now be lengths not divisible by 2.
-        2023-12-20 Modified by Kyle Birch McKay
+        2023-12-20 Modified by Kyle Birch McKay https://github.com/kyle-mckay/random-password
 			- Converted to PowerShell
 			- Added prompt for password Lengh
-			- Generates 10 passwords at a time
+			- Generates $maxGen passwords at a time OR until prompted to stop
      #>
     $conso=@('b','c','d','f','g','h','j','k','l','m','n','p','r','s','t','v','w','x','y','z',"sh","sn","tr","th","ch") #
     $vocal=@('a','e','i','o','u',"ee","oo","ie","au") #
     $speci=@('!','@','#','$','%')
     $password='';
     #srand ((double)microtime()*1000000);
-    $max = $len/4
-    for ($i = 1; $i -le $max; $i++) {
+    $maxGen = $len/4
+    for ($i = 1; $i -le $maxGen; $i++) {
         # Fill half of password with text
         $tempConso = $conso | Get-Random
         $tempVocal = $vocal | Get-Random
@@ -32,10 +32,10 @@ function GetRandomPassword () {
         }
 
     }
-    $max = $len - $password.Length
-    for ($i = 1; $i -le $max; $i++) {
+    $maxGen = $len - $password.Length
+    for ($i = 1; $i -le $maxGen; $i++) {
         #Fill last half of password with numbers
-        if ($spec -and $i -eq $max) {
+        if ($spec -and $i -eq $maxGen) {
             # if special characters wanted, end password with special character
             $tempSpeci = $speci | Get-Random
             $password = $password + $tempSpeci
@@ -52,11 +52,16 @@ function GetRandomPassword () {
     $password = $password.substring(0,1).toupper()+$password.substring(1).tolower()  
     Write-Output $password
     }
-
+#endregion
+    # passwordDelivery = 1 means it will generate $maxGen worth of passwords
+    # passwordDelivery = 2 means it will generate passwords until prompted to stop
+    $passwordDelivery = 1
+    $maxGen = 10
     Clear-Host
     $hostInput = ''
     $repeatLoop = $true
     do {
+        # prompt for password length
         $test = $true
         $hostInput = Read-Host "How many characters do you want your password to be? Minimum length is 8. Press enter while blank to default to 8."
         if ($hostInput -eq '' -or $null -eq $hostInput){
@@ -79,7 +84,21 @@ function GetRandomPassword () {
     } while ($repeatLoop)
 
     $spec = $false
-    for ($i = 1; $i -lt 10; $i++) {
-        GetRandomPassword
+    if($passwordDelivery -eq 1){
+        for ($i = 1; $i -lt $maxGen; $i++) {
+            GetRandomPassword
+        }
+        Pause
     }
-   Pause
+    if ($passwordDelivery -eq 2) {
+        do{
+            #Generate passwords until satisfied
+            GetRandomPassword
+            $hostInput = ''
+            $hostInput = Read-Host "Another? Y/N"
+            $hostInput = $hostInput.ToUpper()
+        } 
+        until($hostInput -eq 'N')
+    }
+
+   
